0c4d4ebcb5e1d116ee3ceb258d41f96126c5724e
Heather Kirk <hskirk@gmail.com>
2019-06-07 01:20:43 +0000

    Automatically backed up by Learn

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -13,12 +13,24 @@ class ArtistsController < ApplicationController
   end 
   
   def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
   end 
   
   def edit
+    @artist = Artist.find(params[:id])
   end 
   
   def update
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
   end 
   
+  private 
+  
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end 
 end 

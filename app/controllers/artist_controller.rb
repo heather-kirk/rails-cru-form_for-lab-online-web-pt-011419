10c879526b42e74e9892751087aa16220cb09f17
Heather Kirk <hskirk@gmail.com>
2019-06-07 01:06:32 +0000

    Automatically backed up by Learn

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,2 +1,22 @@
 class ArtistsController < ApplicationController
+  
+  def index
+    @artists = Artist.all 
+  end 
+  
+  def show
+  end 
+  
+  def new
+  end 
+  
+  def create
+  end 
+  
+  def edit
+  end 
+  
+  def update
+  end 
+  
 end 
